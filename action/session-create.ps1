7c97e2efa3b1cb3df9bfcd456103dae6a3174a0a
s2quake <han0210@netsgo.com>
2025-02-11 17:25:40 +0900

    wip

---
+++ action/session-create.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+    Unjails a validator.
+
+.DESCRIPTION
+    This script unjails a validator using the provided passphrase.
+
+.PARAMETER PassPhrase
+    The secure passphrase for the validator.
+
+.PARAMETER Detailed
+    If specified, provides detailed output.
+
+.EXAMPLE
+    .\validator-unjail.ps1
+
+    Unjails a validator with the provided passphrase.
+
+.EXAMPLE
+    .\validator-unjail.ps1 -Detailed
+
+    Unjails a validator with detailed output.
+#>
+Param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$SessionId,
+    [Parameter(Mandatory = $true, Position = 1)]
+    [string]$PrizeId,
+    [Parameter(Mandatory = $true)]
+    [securestring]$PassPhrase,
+    [switch]$Detailed,
+    [switch]$Watch
+)
+
+$runParameters = @{
+    Name       = "createSession"
+    Arguments  = @{
+        sessionId = $SessionId
+        prize     = $PrizeId
+    }
+    PassPhrase = $PassPhrase
+    Confirm    = $true
+    Watch      = $Watch
+    Detailed   = $Detailed
+}
+./.scripts/run-action.ps1 @runParameters
